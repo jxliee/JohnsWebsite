c9cecb815d54bcfbe393a8cdc01cd3e649c29381
julie smith <jxliesmith@gmail.com>
2023-06-24 15:27:50 -0300

    ahhhhh

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const photos = [
     { src: 'Images/bird.jpg', orientation: 'portrait' },
     { src: 'Images/halifax.jpg', orientation: 'landscape' },
     { src: 'Images/street.jpg', orientation: 'portrait' },
-    // Add more photos as needed
+    // Add more photos as needed by copying the above lines and changing the src and orientation values accordingly
   ];
   
   // Function to create a gallery item with the photo
@@ -20,26 +20,25 @@ const photos = [
   // Function to calculate the appropriate flex-basis for gallery items
   function calculateFlexBasis(orientation) {
     const containerWidth = document.getElementById('gallery').clientWidth;
-    const numItems = Math.floor(containerWidth / 320); // Adjust 320 to your desired width
-    const spacing = (containerWidth - numItems * 320) / (2 * numItems);
-    const landscapeFlexBasis = `calc(${100 / numItems}% - ${2 * spacing}px)`;
+    const numItems = Math.floor(containerWidth / 320); 
+    const spacing = (containerWidth - numItems * 320) / (2 * numItems); //maffs
+    const landscapeFlexBasis = `calc(${200 / numItems}% - ${2 * spacing}px)`;
     const portraitFlexBasis = `calc(2 * ${100 / numItems}% - ${4 * spacing}px)`;
     return orientation === 'landscape' ? landscapeFlexBasis : portraitFlexBasis;
   }
   
   // Function to create the gallery
   function createGallery() {
-    const gallery = document.getElementById('gallery');
-    gallery.innerHTML = '';
+    const gallery = document.getElementById('gallery'); // Get the gallery element
+    gallery.innerHTML = ''; // Clear the gallery element
   
-    photos.forEach((photo) => {
+    photos.forEach((photo) => { // For each photo object in photos, call the createGalleryItem function and append the returned item to the gallery, this will create the gallery
       const item = createGalleryItem(photo);
-      item.style.flexBasis = calculateFlexBasis(photo.orientation);
+      item.style.flexBasis = calculateFlexBasis(photo.orientation); // this will set the flex-basis for the gallery item based on the orientation of the photo
       gallery.appendChild(item);
     });
   }
   
-  // Call the createGallery function when the page loads or whenever the window is resized
+  // Call the createGallery function when the page loads, we will create media queries later to call this function again when the page is resized
   window.addEventListener('DOMContentLoaded', createGallery);
-  window.addEventListener('resize', createGallery);
   
